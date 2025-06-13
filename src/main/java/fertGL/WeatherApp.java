0051b04526c25e245985e4eef2d8fbfb0f3193a5
Gleb <liant379@gmail.com>
2025-06-13 05:48:38 +0300

    init

---
+++ src/main/java/fertGL/WeatherApp.java
@@ -0,0 +1,19 @@
+package fertGL;
+
+import javax.swing.*;
+
+public class WeatherApp extends JFrame {
+    public WeatherApp() {
+        super("Weather App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(450,450);
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(false);
+    }
+}
